405284b5637e69e5b4b6c703a06d16e03f0efba8
Mel Maki <melody.rackham@gmail.com>
2019-10-17 15:19:20 -0600

    Completed MVP

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
-// import Form from './components/Form';
+import Form from './components/Form';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,36 +3,37 @@ import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-const NewUserForm = ({ values, touched, errors }) => {
+const NewUserForm = ({ values, touched, errors, status }) => {
   const [newUser, SetNewUser] = useState([]);
 
   useEffect(() => {
-    status && SetNewUser(newUswer => [...newUser, status]);
+    status && SetNewUser(newUser => [...newUser, status]);
   }, [status]);
+
   return (
     <div className='user-form'>
+      <h1>New User</h1>
       <Form>
         <Field type='text' name='name' placeholder='name' />
         {touched.name && errors.name && <p className='error'>{errors.name}</p>}
         <Field type='text' name='email' placeholder='email' />
         {touched.email && errors.email && <p className='error'>{errors.email}</p>}
-        <Field type='text' name='password' placeholder='password' />
+        <Field type='password' name='password' placeholder='password' />
         {touched.password && errors.password && <p className='error'>{errors.password}</p>}
         <label className='checkbox-container'>
           {' '}
           Terms of Service
-          <Field type='checkbox' name='Terms of Service' checked={values.termsOfService} />
+          <Field type='checkbox' name='Terms' checked={values.Terms} />
           <span className='checkmark' />
         </label>
 
-        <button type='submit'> Submit!</button>
+        <button type='submit'> Submit! </button>
       </Form>
       {newUser.map(user => (
         <ul key={user.id}>
-          <li> Name: {NewUser.name} </li>
-          <li> Email: {NewUser.email} </li>
-          <li> Password: {NewUser.password} </li>
-          <li> TermsOfService: {NewUser.termsOfService} </li>
+          <li> Name: {user.name} </li>
+          <li> Email: {user.email} </li>
+          <li> Password: {user.password.length} characters </li>
         </ul>
       ))}
     </div>
